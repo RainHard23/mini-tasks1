6a5a6eedf4e3009507c22645888cca7ec9442edd
RainHard23 <moiredmi7@gmail.com>
2023-04-05 09:28:56 +0300

    4

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -27,6 +27,7 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        onChange?.(e)
         // задачка на написание онченджа
         onChangeChecked?.(e.currentTarget.checked)
     }
