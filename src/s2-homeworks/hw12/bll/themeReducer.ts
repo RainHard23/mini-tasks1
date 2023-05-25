7414d21cbc8c8dd70f5b7d56d15d115da3a6553e
RainHard23 <moiredmi7@gmail.com>
2023-05-25 05:38:08 +0300

    hw11

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,24 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+    type kek = ReturnType<typeof changeThemeId>
+export const themeReducer = (state = initState, action: kek): typeof state=> { // fix any
     switch (action.type) {
-        // дописать
+        case "SET_THEME_ID":
+            return {
+                ...state,
+                themeId: action.id
+            }
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+   type changeThemeIdType = {
+        type: 'SET_THEME_ID',
+        id: number
+    }
+
+export const changeThemeId = (id: number): changeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
